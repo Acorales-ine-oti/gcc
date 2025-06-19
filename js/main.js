9e39d6eda5405509ac7f5a02148b5da427e8ed73
Acorales-ine-oti <a.corales.ine.informatica@gmail.com>
2025-06-19 13:49:04 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+'use strict'
+
+
+class Usuario{ 
+
+    //Atributos:
+        nomUsu = this.getNombre;
+
+
+    //Metodos:
+
+    constructor(){
+        this.nuevoUsuario()
+    }
+
+
+    nuevoUsuario(){
+        console.log('Hola Mundo');
+    }
+
+}//Fin de la Clase
+
+
+//Instanciación de la clase
+
+    const objUsuario = new Usuario();
